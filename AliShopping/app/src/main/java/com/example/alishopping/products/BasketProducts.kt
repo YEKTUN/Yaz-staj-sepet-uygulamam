21195519237d1d47a775e457b7ebd55740f669d5
ALİ TUNÇER <165657234+YEKTUN@users.noreply.github.com>
2024-08-12 19:26:05 +0300

    Delete AliShopping directory

--- AliShopping/app/src/main/java/com/example/alishopping/products/BasketProducts.kt
+++
@@ -1,9 +0,0 @@
-package com.example.alishopping.products
-
-data class BasketProducts(
-    val basketProductName: String,
-    val basketProductImage: String,
-    val basketProductPrice: String,
-    var basketProductQuantity: Int
-)
-
